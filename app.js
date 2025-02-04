da94187cf7c3dbe704725dc5b1b12e61741ea02c
João pedro <matheusitalo125@gmail.com>
2025-02-04 19:30:49 -0300

    Adiciona Função atualizarListaAmig

--- app.js
+++ app.js
@@ -8,8 +8,25 @@ function adicionarAmigo(){
 
         amigo.value = '';
 
+        atualizarListaAmigos();
 
     }else{
         alert("Por favor, insira um nome!");
+    }
+
+    function atualizarListaAmigos(){
+    	let lista = document.getElementById('listaAmigos');
+
+    	lista.innerHTML = '';
+
+    	for(let i = 0; i < amigos.length; i++){
+    		let elemento = document.createElement('li');
+
+    		let texto = document.createTextNode(amigos[i]);
+
+    		elemento.appendChild(texto);
+
+    		lista.appendChild(elemento);
+    	}
     }
 }
